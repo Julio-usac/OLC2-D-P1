c5b09513b91a31585bb3df534ba51f71a76cf1f5
Julio-Usac <3300473961201@ingenieria.usac.edu.gt>
2022-10-19 12:10:46 -0600

    C3D implementacion if, while y loop

--- parser/nodos/instruccion/InstruccionLoop.py
+++ parser/nodos/instruccion/InstruccionLoop.py
@@ -28,3 +28,12 @@ class InstruccionLoop(Nodo):
             
         return
 
+    def crear_codigo3d(self, tabla_simbolos):
+        # a = expresion
+        self.hojas[2].crear_codigo3d(tabla_simbolos)
+        posicionStack = tabla_simbolos.buscar_posicion(self.hojas[0].nombre)['posicionStack']
+        texto = self.hojas[2].expresion + "\n"
+        self.referencia = self.obtener_temporal()
+        texto += str(self.referencia) + " = P + " + str(posicionStack) + "\n"
+        texto += "stack[(int)" + str(self.referencia) + "]" + " = " + str(self.hojas[2].referencia) + "\n"
+        self.expresion = texto
